a14f075e51320f53c0316eaa506876247df3c5bd
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-27 09:42:05 -0700

    Finished walkthrough 2, built out printConversion class and main class to execute.

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -35,6 +35,12 @@ public class SpeedConverter {
     }
 
     public static void printConversion(double kilometersPerHour) {
-
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        }
+        else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
     }
 }
